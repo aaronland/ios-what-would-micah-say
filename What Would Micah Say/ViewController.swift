d0363f8f217054268c40e8498b1fe235dafac1b2
thisisaaronland <devnull@localhost>
2020-06-11 15:07:31 -0700

    Initial Commit

---
+++ What Would Micah Say/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What Would Micah Say
+//
+//  Created by asc on 6/11/20.
+//  Copyright © 2020 Aaronland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
